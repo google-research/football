e1f9c29f6ecf02a6fe040339e81546607d7c04fa
Victor <3192677+vi3itor@users.noreply.github.com>
2020-03-29 12:22:06 +0900

    Remove function for debugging OpenGL errors

--- third_party/gfootball_engine/src/systems/graphics/rendering/opengl_renderer3d.cpp
+++ third_party/gfootball_engine/src/systems/graphics/rendering/opengl_renderer3d.cpp
@@ -44,29 +44,6 @@
 #include <wingdi.h>
 #endif
 
-// Debug OpenGl errors, taken from https://learnopengl.com/In-Practice/Debugging
-GLenum glCheckError_(const char *file, int line)
-{
-    GLenum errorCode;
-    while ((errorCode = glGetError()) != GL_NO_ERROR)
-    {
-        std::string error;
-        switch (errorCode)
-        {
-            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
-            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
-            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
-            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
-            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
-            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
-        }
-        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
-    }
-    return errorCode;
-}
-//To debug, replace ALL occurrences of DO_VALIDATION with DO_VALIDATION_ in current file and uncomment the following line
-//#define DO_VALIDATION glCheckError_(__FILE__, __LINE__);
 
 namespace blunted {
 
