68609527f99e296bc0ee66c0912c6bd7d875354a
Tyler Rarick <tyler.rarick@gmail.com>
2021-04-20 15:39:22 +0200

    Added example config and an experimental config.py. Trying another direction with it.

--- partyball/env/config.py
+++ partyball/env/config.py
@@ -1,7 +1,18 @@
+import yaml
+
 from gfootball.env import config
+
 
 class PartyConfig(config.Config):
 
   def __init__(self, values=None):
     # Will probably need to do some updating here eventually
     super().__init__(values)
+
+  def from_yaml(file_path):
+    with open(file_path, 'r') as stream:
+      yaml_data = yaml.safe_load(stream)
+
+    party_config = PartyConfig()
+
+    return PartyConfig()
---
+++ partyball/env/config_test.py
@@ -0,0 +1,7 @@
+import config
+
+
+def test_config_loader():
+  # TODO: Add a test function
+  # config.PartyConfig.from_yaml(path)
+  pass
