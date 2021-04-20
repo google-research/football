f48e9898ae96d08a538bff8b59ab21f87ff8e114
Tyler Rarick <tyler.rarick@gmail.com>
2021-04-20 13:31:48 +0200

    Added simply config override class

---
+++ partyball/env/config.py
@@ -0,0 +1,7 @@
+from gfootball.env import config
+
+class PartyConfig(config.Config):
+
+  def __init__(self, values=None):
+    # Will probably need to do some updating here eventually
+    super().__init__(values)
